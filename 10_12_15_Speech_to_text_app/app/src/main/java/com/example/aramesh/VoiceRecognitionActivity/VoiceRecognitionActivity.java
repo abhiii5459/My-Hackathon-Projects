f8d153cd81d29fa8a0b2e8ce5de8c49f82aa9c50
Abhiram R <abhi.darkness@gmail.com>
2018-02-12 23:30:42 +0530

    Added Speech to Text App - 10_12_15

---
+++ 10_12_15_Speech_to_text_app/app/src/main/java/com/example/aramesh/VoiceRecognitionActivity/VoiceRecognitionActivity.java
@@ -0,0 +1,200 @@
+package com.example.aramesh.VoiceRecognitionActivity;
+
+/**
+ * Created by aramesh on 12/09/15.
+ */
+
+
+import java.io.File;
+import java.util.ArrayList;
+        import java.util.List;
+
+        import android.app.Activity;
+        import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.speech.RecognizerIntent;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ListView;
+        import android.widget.Spinner;
+        import android.widget.Toast;
+
+import com.example.aramesh.VoiceRecognitionActivity.VoiceRecognitionActivity;
+
+public class VoiceRecognitionActivity extends Activity {
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
+
+    private EditText metTextHint;
+    private ListView mlvTextMatches;
+    private Spinner msTextMatches;
+    private Button mbtSpeak;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy old = StrictMode.getThreadPolicy();
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder(old)
+                .permitDiskWrites()
+                .build());
+        StrictMode.setThreadPolicy(old);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_recognition);
+        metTextHint = (EditText) findViewById(R.id.etTextHint);
+        mlvTextMatches = (ListView) findViewById(R.id.lvTextMatches);
+        msTextMatches = (Spinner) findViewById(R.id.sNoOfMatches);
+        mbtSpeak = (Button)findViewById(R.id.btSpeak);
+        mbtSpeak.performClick();
+        checkVoiceRecognition();
+    }
+
+    public void checkVoiceRecognition() {
+        // Check if voice recognition is present
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
+                RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() == 0) {
+            mbtSpeak.setEnabled(false);
+            mbtSpeak.setText("Voice recognizer not present");
+            Toast.makeText(this, "Voice recognizer not present",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void speak(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        // Specify the calling package to identify your application
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
+                .getPackage().getName());
+
+        // Display an hint to the user about what he should say.
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, metTextHint.getText()
+                .toString());
+
+
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en-IN");
+
+
+
+        int noOfMatches = Integer.parseInt(msTextMatches.getSelectedItem()
+                .toString());
+
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, noOfMatches);
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE)
+
+            //If Voice recognition is successful then it returns RESULT_OK
+            if (resultCode == RESULT_OK) {
+
+                ArrayList<String> textMatchList = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                if (!textMatchList.isEmpty())
+                {
+                    // If first Match contains the 'search' word
+                    // Then start web search.
+                    if (textMatchList.get(0).contains("currency"))
+                    {
+                        //Launch camera
+//                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//
+//                        File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
+//                        imagesFolder.mkdirs(); // <----
+//                        File image = new File(imagesFolder, "image_001.jpg");
+//                        Uri uriSavedImage = Uri.fromFile(image);
+//
+//                        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+//
+                        Intent intent = getPackageManager().getLaunchIntentForPackage("com.sg.currency");
+                        if (intent != null) {
+                            // We found the activity now start the activity
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        } else {
+                            // Bring user to the market or let them choose an app?
+                            intent = new Intent(Intent.ACTION_VIEW);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            intent.setData(Uri.parse("market://details?id=" + "com.sg.currency"));
+                            startActivity(intent);
+                        }
+                    }
+                    if (textMatchList.get(0).contains("search")) {
+
+                        String searchQuery = textMatchList.get(0);
+                        searchQuery = searchQuery.replace("search", "");
+                        Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
+                        search.putExtra(SearchManager.QUERY, searchQuery);
+                        startActivity(search);
+                    } else {
+                        // populate the Matches
+                        mlvTextMatches
+                                .setAdapter(new ArrayAdapter<String>(this,
+                                        android.R.layout.simple_list_item_1,
+                                        textMatchList));
+                    }
+
+
+
+                    if (textMatchList.get(0).contains("next imeeting")) {
+
+                        String searchQuery = textMatchList.get(0);
+                        Context context = getApplicationContext();
+                        CharSequence text = searchQuery.substring(searchQuery.lastIndexOf(" ")+1);; //"Hello toast!";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+
+                    } else {
+                        // populate the Matches
+                        mlvTextMatches
+                                .setAdapter(new ArrayAdapter<String>(this,
+                                        android.R.layout.simple_list_item_1,
+                                        textMatchList));
+                    }
+
+
+
+
+
+
+
+                }
+                //Result code for various error.
+            } else if (resultCode == RecognizerIntent.RESULT_AUDIO_ERROR) {
+                showToastMessage("Audio Error");
+            } else if (resultCode == RecognizerIntent.RESULT_CLIENT_ERROR) {
+                showToastMessage("Client Error");
+            } else if (resultCode == RecognizerIntent.RESULT_NETWORK_ERROR) {
+                showToastMessage("Network Error");
+            } else if (resultCode == RecognizerIntent.RESULT_NO_MATCH) {
+                showToastMessage("No Match");
+            } else if (resultCode == RecognizerIntent.RESULT_SERVER_ERROR) {
+                showToastMessage("Server Error");
+            }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    /**
+     * Helper method to show the toast message
+     **/
+    void showToastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
